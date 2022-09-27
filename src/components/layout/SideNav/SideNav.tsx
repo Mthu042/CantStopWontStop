b7c71e95763b621e6063818605f793dec2b28062
matvey.gershman <matvey767@gmail.com>
2022-09-27 18:25:17 +0300

    delete deprecated (SFC => FC)

--- src/components/layout/SideNav/SideNav.tsx
+++ src/components/layout/SideNav/SideNav.tsx
@@ -5,7 +5,7 @@ import { WithStyle } from '@medly-components/utils';
 import React, { useCallback } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 
-export const SideNav: React.SFC & WithStyle = React.memo(() => {
+export const SideNav: React.FC & WithStyle = React.memo(() => {
     const { pathname } = useLocation(),
         history = useHistory(),
         handlePathChange = useCallback((page: string) => history.push(page), [history]);
